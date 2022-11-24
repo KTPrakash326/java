085fb1f3d0b97e62a88c8d53d5c762b52da97fe3
KTPrakash326 <prakashkt326@gmail.com>
2022-11-24 17:04:59 +0530

    childuuu

--- StringExample.java
+++ StringExample.java
@@ -4,7 +4,7 @@ public class StringExample {
 
 	public static void main(String[] args) {
 	
-		String name="Dog";
+		String name="childuuuuuuuu2";
 		for(int i=0;i<name.length();i++)
 		{
 			if(name.charAt(i)==name.charAt(i+1))
