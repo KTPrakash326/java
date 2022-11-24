df0d2975403e0b305a29e2a686f0f8614443bbe2
KTPrakash326 <118660749+KTPrakash326@users.noreply.github.com>
2022-11-24 16:39:10 +0530

    Update StringExample.java

--- StringExample.java
+++ StringExample.java
@@ -4,7 +4,7 @@ public class StringExample {
 
 	public static void main(String[] args) {
 	
-		String name="pradeep  prakash sameer megastar ROCKSTAR";
+		String name="child";
 		for(int i=0;i<name.length();i++)
 		{
 			if(name.charAt(i)==name.charAt(i+1))
