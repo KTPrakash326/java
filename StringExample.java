d93f458de8eb9e78ec5478ca12fe49ad97a198e6
KTPrakash326 <118660749+KTPrakash326@users.noreply.github.com>
2022-11-24 14:54:19 +0530

    cental repo change commit

--- StringExample.java
+++ StringExample.java
@@ -4,7 +4,7 @@ public class StringExample {
 
 	public static void main(String[] args) {
 	
-		String name="pradeep";
+		String name="pradeep  prakash";
 		for(int i=0;i<name.length();i++)
 		{
 			if(name.charAt(i)==name.charAt(i+1))
